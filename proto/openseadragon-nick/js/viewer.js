cdd896f3de2e37c07d7182e9bc0765f10bbc3477
Nicholas Verrochi <verrochi92@gmail.com>
2023-03-01 12:01:51 -0500

    got coordinate display working with openseadragon

--- proto/openseadragon-nick/js/viewer.js
+++ proto/openseadragon-nick/js/viewer.js
@@ -5,11 +5,26 @@
     For CS410 - The Axolotl Project
 */
 
-window.onload = function() {
+window.onload = function () {
     viewer = OpenSeadragon({
         id: "openseadragon-viewer",
         visibilityRation: 1.0,
         prefixUrl: "https://openseadragon.github.io/openseadragon/images/",
         tileSources: "../../data/W255B_0.dzi"
     });
+
+
+    var tracker = new OpenSeadragon.MouseTracker({
+        element: viewer.container,
+        moveHandler: function (event) {
+            var webPoint = event.position;
+            var viewportPoint = viewer.viewport.pointFromPixel(webPoint);
+            var imagePoint = viewer.viewport.viewportToImageCoordinates(viewportPoint);
+
+            var display = document.getElementById("coordinates");
+            display.innerText = "Coordinates: " + imagePoint.toString();
+        }
+    });
+
+    tracker.setTracking(true);
 }
